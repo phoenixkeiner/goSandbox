4937dbf3dc2d048b06bbc43d11b06ce5dd6f1187
phoenixkeiner <110541226+phoenixkeiner@users.noreply.github.com>
2025-04-22 15:45:12 -0400

    initial go 'hello world'

---
+++ hello.go
@@ -0,0 +1,10 @@
+// Decalare a main package
+package main
+
+// Import fmt packege for formatted text
+import "fmt"
+
+func main() {
+	// Print Hello, World! to the console
+	fmt.Println("Hello, World!")
+}
