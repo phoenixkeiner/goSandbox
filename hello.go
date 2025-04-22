98da60360f15f59a832dbd4f29000b699ee07588
phoenixkeiner <110541226+phoenixkeiner@users.noreply.github.com>
2025-04-22 15:54:24 -0400

    ability to print time

--- hello.go
+++ hello.go
@@ -1,10 +1,16 @@
 // Decalare a main package
 package main
 
-// Import fmt packege for formatted text
-import "fmt"
+// Import packages
+import (
+	"fmt"  // ability to print
+	"time" // time
+)
 
 func main() {
 	// Print Hello, World! to the console
 	fmt.Println("Hello, World!")
+
+	// Print the time
+	fmt.Println("The time is: ", time.Now())
 }
